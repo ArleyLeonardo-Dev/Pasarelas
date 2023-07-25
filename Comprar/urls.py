856e0cad766b909f7d8290a329edf7852cfffadd
Yurei <arleyleonardodev@gmail.com>
2023-07-25 14:19:04 -0500

    Linkeo entre las diferentes paginas de compra

--- Comprar/urls.py
+++ Comprar/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('', home, name = 'home')
+	path('', home, name = 'home'),
+	
+	#Paginas para obtener tokens
+	path('token_basic/', token_basic, name = 'token_basic'),
+	path('token_normal/', token_normal, name = 'token_normal'),
+	path('token_premiun/', token_premiun, name = 'token_premiun'),
+
+	#Paginas de formulario de pagos
+	path('token_basic/CompraBasic/', CompraBasic, name = 'CompraBasic'),
+	path('token_normal/CompraNormal/', CompraNormal, name = 'CompraNormal'),
+	path('token_premiun/CompraPremiun/', CompraPremiun, name = 'CompraPremiun')
 ]
--- Comprar/views.py
+++ Comprar/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def home(request):
+	
 	return render(request, 'home.html')
+
+#Tokens
+def token_basic(request):
+
+	return redirect('CompraBasic/')
+
+
+def token_normal(request):
+
+	return redirect('CompraNormal')
+
+def token_premiun(request):
+
+	return redirect('CompraPremiun')
+
+
+#Pagina de compras
+def CompraBasic(request):
+
+	return render(request, 'CompraBasic.html')
+
+
+def CompraNormal(request):
+
+	return render(request, 'CompraNormal.html')
+
+
+def CompraPremiun(request):
+
+	return render(request, 'CompraPremiun.html')
